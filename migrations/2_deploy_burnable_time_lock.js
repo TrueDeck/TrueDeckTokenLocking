f85ebbd4418bf448d09d1f15063aba9ca46bbdb1
Akshat Nigam <akshat.dev.zero@gmail.com>
2019-04-04 14:23:35 +0530

    Migration script corrected.

--- migrations/2_deploy_burnable_time_lock.js
+++ migrations/2_deploy_burnable_time_lock.js
@@ -3,6 +3,7 @@ var BurnableTimelock = artifacts.require("BurnableTimelock");
 module.exports = function(deployer, network, accounts) {
     var tdpTokenAddress = "0x5b11aAcB6Bddb9ffab908FDCE739Bf4aed554327";
     var releaseTime = 1648771200;   // Friday, April 1, 2022 12:00:00 AM UTC
+    var releaseDelay = 864000;      // 10 days
 
     if (network === "ropsten") {
         tdpTokenAddress = "0x861B765D069bb08C4c8fC294CebfB4e85d2cA8B0";
@@ -13,6 +14,7 @@ module.exports = function(deployer, network, accounts) {
     console.log(`  - TOKEN = ${tdpTokenAddress}`);
     console.log(`  - FROM  = ${accounts[0]}`);
     console.log(`  - TIME  = ${releaseTime}`);
+    console.log(`  - DELAY = ${releaseDelay}`);
 
-    deployer.deploy(BurnableTimelock, tdpTokenAddress, accounts[0], releaseTime);
+    deployer.deploy(BurnableTimelock, tdpTokenAddress, accounts[0], releaseTime, releaseDelay);
 };
